50e5aa7df5f0b2a0c8bb2335366d897e1ee032a5
i42gapod <i42gapod@uco.es>
2023-05-01 18:15:14 +0200

    Now with strcat()

--- include/myinclude.h
+++ include/myinclude.h
@@ -1,4 +1,7 @@
 #include <stdio.h>
 void f(){
-    printf("Hello world \n");
+    char c1[100]= "Hello world";
+    char c2[100]= ", I am Dalia García";
+    printf("%s\n", strcat(c1, c2));
+    return 0;
 }
